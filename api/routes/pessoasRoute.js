498b536e64e8e42f5aa82535a18c35caaf383d2f
plinio <plinio.garcia@ifc.edu.br>
2021-04-20 10:26:29 -0300

    implementando demais metodos de restaurar

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -11,6 +11,8 @@ router.delete('/pessoas/:id', PessoaController.apagaPessoa)
 router.get('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.pegaUmaMatricula)
 router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
 router.post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
+router.post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
+router.post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
 router.put('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.atualizaMatricula)
 router.delete('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.apagaMatricula)
 
