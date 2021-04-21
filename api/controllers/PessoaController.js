d7ba823160955fc4e92b353e37e49e01ab766c4a
plinio <plinio.garcia@ifc.edu.br>
2021-04-21 15:13:19 -0300

    usando transações (transactions)

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -199,12 +199,15 @@ class PessoaController {
   static async cancelaPessoa(req, res) {
     const {estudanteId} = req.params
     try {
-      await database.Pessoas
-        .update({ativo: false}, { where: {id: Number(estudanteId) }})
+      database.sequelize.transaction(async transacao => {
+        await database.Pessoas
+        .update({ativo: false}, { where: {id: Number(estudanteId) }}, {transaction: transacao})
 
       await database.Matriculas
-        .update({status: 'cancelado'}, { where: {estudante_id: Number(estudanteId) }})
+        .update({status: 'cancelado'}, { where: {estudante_id: Number(estudanteId) }}, {transaction: transacao})
         return res.status(200).json({message: `Matrículas do estudante id ${estudanteId} canceladas.`})
+      })
+      
 
     } catch (error) {
       return res.status(500).json(error.message)
